240c51224e2f97e17d12057fa8ab361a44126b6f
Christian Dahlqvist <acd@Christians-MacBook-Pro.local>
2017-05-19 08:59:38 +0100

    Modified query

--- metrics/runners/influxdb_database_size_runner.py
+++ metrics/runners/influxdb_database_size_runner.py
@@ -3,6 +3,7 @@ from influxdb import InfluxDBClient
 import os
 import logging
 import time
+import json
 
 logger = logging.getLogger("track.metrics")
 
@@ -67,11 +68,11 @@ class InfluxDBDatabaseSizeRunner:
             "index_pattern": database
         }
 
-        query = 'SELECT "diskBytes", "writePointsOk" FROM "shard" WHERE "database"="{}" GROUP BY "database" ORDER BY time DESC LIMIT 1'.format(database)
+        query = 'SELECT diskBytes, writePointsOk FROM "shard" GROUP BY "database" ORDER BY time DESC LIMIT 1'.format(database)
 
         logger.info("[InfluxDBDatabaseSizeRunner] Query: {}".format(query))
 
-        response = self._influxdb_client.query(query, database="_internal").raw
+        response = json.loads(self._influxdb_client.query(query, database="_internal").raw)
 
         logger.info("[InfluxDBDatabaseSizeRunner] Response: {}".format(response))
 
